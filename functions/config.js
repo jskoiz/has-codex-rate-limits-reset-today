44613f7e26215e99c336e516a263753493a75ac8
jskoiz <20649937+jskoiz@users.noreply.github.com>
2026-03-13 23:01:14 -1000

    Serve config from a Pages function

---
+++ functions/config.js
@@ -0,0 +1,11 @@
+export const onRequestGet = async (context) => {
+  const response = await context.env.ASSETS.fetch(new URL("/config.html", context.request.url));
+  const headers = new Headers(response.headers);
+  headers.set("x-robots-tag", "noindex, nofollow");
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
+};
